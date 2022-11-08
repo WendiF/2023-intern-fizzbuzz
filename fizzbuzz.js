638faa4a50adeab23ea5ae0502a40f7dcc41b82b
Muhanad Al-Khafaji <Muhanad.Al-Khafaji@softwire.com>
2022-11-08 17:51:48 +0000

    Added for loop and if statements, appropriately flagging multiples of 3, 5 and 15. Prints numbers from 1 to 100, replacing those multiples with key words.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,19 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
-
-    // Put your code here...
+    // Iterate from 1 to 100
+    for (let i = 1; i < 101; i++) {
+        if (i % 15 == 0) {
+            // Case where number is multiple of 3 and 5
+            console.log("fizzbuzz")
+        } else if (i % 3 == 0) {
+            // Case where number is multiple of 3
+            console.log("fizz")
+        } else if (i % 5 == 0) {
+            // Case where number is multiple of 5
+            console.log("buzz")
+        } else
+        console.log(i);
+    }
 
 }
 
