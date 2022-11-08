9689ef6dfc900e2bfa60e021aa59ed7e18e4ad69
Stuart Knowles <Stuart.Knowles@softwire.com>
2022-11-08 17:22:38 +0000

    Basic Fizzbuzz game

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,22 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
+    console.log("Let's play FizzBuzz!");
 
-    // Put your code here...
+    const rules = {
+        3:'Fizz',
+        5:'Buzz',
+        /*7:'Bang',
+        11:'Bong',
+        13:'Fezz'*/
+    }
+    for (let i = 1; i < 101; i++) {
+        let out = ""
+        for (const key in rules) {
+            if (!(i % key)) out += rules[key]
+        }
+        if (out) console.log(out)
+        else console.log(i)
+    }
 
 }
 
