cf96b0c5e1eab1e8b29e7369d7768bce14fa5bda
Stuart Knowles <Stuart.Knowles@softwire.com>
2022-11-09 10:32:08 +0000

    Fizzbuzz with additional rules

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,25 +1,80 @@
 // This is our main function
-function fizzbuzz() {
+function fizzbuzz(N) {
     console.log("Let's play FizzBuzz!");
 
-    const rules = {
-        3:'Fizz',
-        5:'Buzz',
-        /*7:'Bang',
-        11:'Bong',
-        13:'Fezz'*/
+    function fizz(list) {
+        list.push('Fizz')
+        return list
     }
-    for (let i = 1; i < 101; i++) {
-        let out = ""
-        for (const key in rules) {
-            if (!(i % key)) out += rules[key]
+
+    function buzz(list) {
+        list.push('Buzz')
+        return list
+    }
+
+    function bang(list) {
+        list.push('Bang')
+        return list
+    }
+
+    function bong(list) {
+        list = ['Bong']
+        return list
+    }
+
+    function fezz(list) {
+        const first_B = list.findIndex((string) => string.slice(0,1) == 'B')
+        if (first_B == -1) {
+            list.push('Fezz')
         }
-        if (out) console.log(out)
-        else console.log(i)
+        else {
+            const start = list.slice(0, first_B)
+            const end = list.slice(first_B)
+            list = start.concat(['Fezz'], end)
+        }
+        return list
+    }
+
+    function reverse(list) {
+        list.reverse()
+        return list
+    }
+
+    const rules = {
+        3:fizz,
+        5:buzz,
+        7:bang,
+        11:bong,
+        13:fezz,
+        17:reverse
+    }
+    for (let i = 1; i <= N; i++) {
+        let out = []
+        for (const key in rules) {
+            if (!(i % key)) {
+                out = rules[key](out)
+            }
+        }
+        if (out.length > 0) {
+            console.log(i + ': ' + out.join(''))
+        }
+        else console.log(i + ': ' + i)
     }
 
 }
 
+/*function get_user_input() {
+    let input = prompt("How high will we play FizzBuzz to?: ")
+    let N = parseInt(input)
+    while (N == NaN){
+        input = prompt("I'm sorry, that's not a number, please try again: ")
+        N = parseInt(input)
+    }
+    return N
+}
+
 // Now, we run the main function:
-fizzbuzz();
+const N = get_user_input(N)*/
+const N = 300
+fizzbuzz(N);
 
