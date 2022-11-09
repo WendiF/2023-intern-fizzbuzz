dd54197cbdd481d6eedc1b4d42e56bc6b1c732a4
Muhanad Al-Khafaji <Muhanad.Al-Khafaji@softwire.com>
2022-11-09 09:16:57 +0000

    Moved checks for 3,5,7 into separate function.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,20 +3,8 @@ function fizzbuzz() {
     // Iterate from 1 to 100
     for (let i = 1; i < 101; i++) {
         let answer = "";
-        // Check if number is an identified multiple
-        if (i % 3 == 0) {
-            // Case where number is a multiple of 3
-            answer = answer.concat("Fizz");
-        }
-        if (i % 5 == 0) {
-            // Case where number is a multiple of 5
-            answer = answer.concat("Buzz");
-        }
-        if (i % 7 == 0) {
-            // Case where number is a multiple of 7
-            answer = answer.concat("Bang");
-        }
-
+        // Append strings for numbers that are identified multiples
+        answer = further_multiples(i, answer);
         // Print number or phrase
         if (answer.length > 0) {
             // Case where number is a multiple
@@ -26,7 +14,23 @@ function fizzbuzz() {
             console.log(i);
         }
     }
+}
 
+// Function to check for multiples whose associated strings can be appended
+function further_multiples(i, answer) {
+    if (i % 3 == 0) {
+        // Case where number is a multiple of 3
+        answer = answer.concat("Fizz");
+    }
+    if (i % 5 == 0) {
+        // Case where number is a multiple of 5
+        answer = answer.concat("Buzz");
+    }
+    if (i % 7 == 0) {
+        // Case where number is a multiple of 7
+        answer = answer.concat("Bang");
+    }
+    return answer
 }
 
 // Now, we run the main function:
